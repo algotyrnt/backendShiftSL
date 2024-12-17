d4652eaa7e4740d1f96d41b4ec753142a217db8c
algotyrnt <punjitha@gmail.com>
2024-12-17 16:14:32 +0530

    project created
    Signed-off-by: algotyrnt <punjitha@gmail.com>

---
+++ src/main/java/com/shiftsl/shiftslbackend/ShiftSLbackendApplication.java
@@ -0,0 +1,13 @@
+package com.shiftsl.shiftslbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShiftSLbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShiftSLbackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shiftsl/shiftslbackend/ShiftSLbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shiftsl.shiftslbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShiftSLbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
