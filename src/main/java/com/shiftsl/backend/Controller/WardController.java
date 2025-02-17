d3e81f54a1b5fd1bb92852bd05649c2b5ecd3291
algotyrnt <punjitha@gmail.com>
2025-02-17 18:01:15 +0530

    Ward controller created

---
+++ src/main/java/com/shiftsl/backend/Controller/WardController.java
@@ -0,0 +1,13 @@
+package com.shiftsl.backend.Controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/ward")
+@AllArgsConstructor
+public class WardController {
+}
