bee37c9be54c6e05de1d3e878e4ec189e7eb29e7
algotyrnt <x@tyrnt.co>
2025-03-17 10:15:43 +0530

    shift repo anotated + leave repo created

--- src/main/java/com/shiftsl/backend/repo/ShiftRepo.java
+++ src/main/java/com/shiftsl/backend/repo/ShiftRepo.java
@@ -2,9 +2,11 @@ package com.shiftsl.backend.repo;
 
 import com.shiftsl.backend.model.Shift;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface ShiftRepo extends JpaRepository<Shift, Long> {
     List<Shift> findByShiftAvailable(boolean shiftAvailable);
 }
