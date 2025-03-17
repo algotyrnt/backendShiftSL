bdaf79493abf008e1935e32695e6b6d5d58dcd55
algotyrnt <x@tyrnt.co>
2025-03-17 12:05:14 +0530

    api points updated

--- src/main/java/com/shiftsl/backend/Controller/ShiftController.java
+++ src/main/java/com/shiftsl/backend/Controller/ShiftController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/shift")
+@RequestMapping("/api/shift")
 @AllArgsConstructor
 public class ShiftController {
 
