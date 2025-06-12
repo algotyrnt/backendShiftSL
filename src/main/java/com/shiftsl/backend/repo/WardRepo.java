79b7cf31fe419ae0561fd1c474b402687b93f77e
punjitha <punjitha@gmail.com>
2025-06-12 11:58:11 +0530

    ward graphQL queries completed, WardRepo cleaned

--- src/main/java/com/shiftsl/backend/repo/WardRepo.java
+++ src/main/java/com/shiftsl/backend/repo/WardRepo.java
@@ -3,12 +3,12 @@ package com.shiftsl.backend.repo;
 import com.shiftsl.backend.model.Ward;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.graphql.data.GraphQlRepository;
-import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
-@Repository
 @GraphQlRepository
 public interface WardRepo extends JpaRepository<Ward, Long> {
     Optional<Ward> findByName(String name);
+    List<Ward> findByWardAdmin_Id(Long wardAdminId);
 }
