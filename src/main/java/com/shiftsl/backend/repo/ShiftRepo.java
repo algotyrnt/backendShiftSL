de38c56e1a9ee243209cd372f5e89daee908a8d7
punjitha <punjitha@gmail.com>
2025-06-12 15:56:25 +0530

    shift query added to the schema file

--- src/main/java/com/shiftsl/backend/repo/ShiftRepo.java
+++ src/main/java/com/shiftsl/backend/repo/ShiftRepo.java
@@ -6,13 +6,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
+import org.springframework.graphql.data.GraphQlRepository;
 
 import java.util.List;
 import java.util.Optional;
 import java.time.LocalDateTime;
 
-@Repository
+@GraphQlRepository
 public interface ShiftRepo extends JpaRepository<Shift, Long> {
     @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query("SELECT s FROM Shift s WHERE s.id = :shiftID")
