67c7dbfe7b4fa60f2c64eb3d68e4f0b676d81922
punjitha <punjitha@gmail.com>
2025-06-12 11:40:00 +0530

    query methods created for ward

--- src/main/java/com/shiftsl/backend/repo/WardRepo.java
+++ src/main/java/com/shiftsl/backend/repo/WardRepo.java
@@ -2,11 +2,13 @@ package com.shiftsl.backend.repo;
 
 import com.shiftsl.backend.model.Ward;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.graphql.data.GraphQlRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
+@GraphQlRepository
 public interface WardRepo extends JpaRepository<Ward, Long> {
     Optional<Ward> findByName(String name);
 }
