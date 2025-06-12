a8a789077826674a3fa47139f87493aed2e14e33
punjitha <punjitha@gmail.com>
2025-06-12 11:59:18 +0530

    user repo cleaned

--- src/main/java/com/shiftsl/backend/repo/UserRepo.java
+++ src/main/java/com/shiftsl/backend/repo/UserRepo.java
@@ -4,12 +4,10 @@ import com.shiftsl.backend.model.Role;
 import com.shiftsl.backend.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.graphql.data.GraphQlRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
-@Repository
 @GraphQlRepository
 public interface UserRepo extends JpaRepository<User, Long> {
     Optional<User> findByPhoneNo(String phoneNo);
