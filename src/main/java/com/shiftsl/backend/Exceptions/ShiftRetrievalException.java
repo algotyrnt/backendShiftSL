dca442f06ef31d4fbb33d59f9745704e664fc583
EranTimothy-dev <eranperera.dev@gmail.com>
2025-06-13 08:15:55 +0530

    improved exception handling for shift service and added logging

---
+++ src/main/java/com/shiftsl/backend/Exceptions/ShiftRetrievalException.java
@@ -0,0 +1,7 @@
+package com.shiftsl.backend.Exceptions;
+
+public class ShiftRetrievalException extends RuntimeException{
+    public ShiftRetrievalException(String message) {
+        super(message);
+    }
+}
